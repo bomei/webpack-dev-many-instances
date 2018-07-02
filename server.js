231184cc30810b52263513381193b737154ec100
zannb <zannb@outlook.com>
2018-07-02 18:52:49 +0800

    update modules; hot-update.json 404

--- server.js
+++ server.js
@@ -6,31 +6,34 @@ const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
-const config2 = require('./webpack.config2')
-const compiler2 = webpack(config2)
+// const config2 = require('./webpack.config2')
+// const compiler2 = webpack(config2)
 // __webpack_public_path__ = `http://localhost/vue`
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
-app.use(webpackDevMiddleware(compiler2, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-app.use(require("webpack-hot-middleware")(compiler2,{
-  log: false,
-  path:"/key2/_whaaat",
-}));
-
-// app.use(webpackDevMiddleware(compiler, {
-//   noInfo: true,
+// app.use(webpackDevMiddleware(compiler2, {
+  // noInfo: true,
 //   publicPath: config.output.publicPath
 // }));
 
-// app.use(require("webpack-hot-middleware")(compiler,{
+// app.use(require("webpack-hot-middleware")(compiler2,{
 //   log: false,
-//   path:"/key1/_whaaat",
+//   path:"/key2/_whaaat",
 // }));
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  // lazy: true
+  // publicPath: config.output.publicPath
+}));
+
+app.use(require("webpack-hot-middleware")(compiler,{
+  log: false,
+  path:"/_whaaat",
+  // hotUpdateChunkFilename: '[hash].hot-update.js',
+  // hotUpdateMainFilename:'[hash].hot-update.json'
+}));
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack')
 
-module.exports = {
+let config1 = {
+  mode: 'development',
   entry: {
-    main:['./src/key1/index.js','webpack-hot-middleware/client?path=/key1/_whaaat']
+    main:['./src/key1/index.js','webpack-hot-middleware/client?path=/_whaaat']
     // index:['./index.html','webpack-hot-middleware/client']
   },
   devtool: 'inline-source-map',
@@ -31,8 +32,45 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist','key1'),
-    publicPath: 'http://localhost:3000/key1',
+    publicPath: 'http://localhost:3000/key1/',
     // hotUpdateChunkFilename: 'hot-update.js',
     // hotUpdateMainFilename: 'hot-update.json',
   }
 };
+
+let config2= {
+  mode: 'development',
+  entry: {
+    main:['./src/key2/index.js','webpack-hot-middleware/client?path=/_whaaat']
+    // index:['./index.html','webpack-hot-middleware/client']
+  },
+  devtool: 'inline-source-map',
+  devServer:{
+    contentBase: './dist',
+    hot: true
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist/key2']),
+    new HtmlWebpackPlugin({
+      // title: 'Bo webpack',
+      template: 'index.html',
+      inject: true,
+    }),
+    
+    // OccurenceOrderPlugin is needed for webpack 1.x only
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    // Use NoErrorsPlugin for webpack 1.x
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist','key2'),
+    publicPath: 'http://localhost:3000/key2/',
+    // hotUpdateChunkFilename: 'hot-update.js',
+    // hotUpdateMainFilename: 'hot-update.json',
+  }
+};
+
+module.exports = [config1, config2]
